96dc02790e45e475e07db786e789df137fae5422
Chang-Young Park <parkcy0630@gmail.com>
2024-05-14 00:20:18 +0900

    API Test -240514

--- react-and-api/src/App.js
+++ react-and-api/src/App.js
@@ -1,7 +1,53 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const URL =
+  "http://apis.data.go.kr/6430000/cbRecreationalForestInfoService/getRecreationalForestInfo?serviceKey=";
+
+const serviceKey = process.env.REACT_APP_API_KEY; // Get the API key from environment variables
+
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      setError(null);
+      setData(null);
+      setLoading(true);
+
+      const response = await axios.get(URL, {
+        params: {
+          serviceKey: serviceKey,
+          currentPage: 1,
+          perPage: 1,
+        },
+      });
+      setData(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log("Service Key:", serviceKey); // Log the service key to the console
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
+  if (!data) return null;
+
+  console.log(data);
+  //const name = data.body.items[0].NM;
+
   return (
-    <div>
-      <h1>hello New World!</h1>
+    <div className="App">
+      <p>길이 : {data.body.length}</p>
+      {/* <p>이름 : {data.header.perPage}</p> */}
     </div>
   );
 }
