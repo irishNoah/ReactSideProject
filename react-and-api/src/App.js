008dcdfb3ae9180d7e166307427d06d10de90155
Chang-Young Park <parkcy0630@gmail.com>
2024-05-28 21:39:50 +0900

    240528 - Install React Router Dom + CSS hover

--- react-and-api/src/App.js
+++ react-and-api/src/App.js
@@ -1,55 +1,22 @@
-import React, { useState, useEffect } from "react";
-import styles from "./App.module.css";
-
-function TrainFetchApi() {
-  const [coin, setCoin] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function fetchCoinData() {
-      try {
-        const response = await fetch("https://api.coinpaprika.com/v1/tickers");
-        const coins = await response.json();
-
-        setCoin(coins); // >>> 모두 다 불러옴
-      } catch (error) {
-        setError(error);
-        console.error("Error fetching coin data:", error);
-      }
-    }
-
-    fetchCoinData();
-  }, []);
-
-  if (error) {
-    return <div>Error fetching coin data: {error.message}</div>;
-  }
-
-  if (!coin) {
-    return <div>Loading...</div>;
-  }
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import HomeCoin from "./routes/HomeCoin";
+function App() {
   return (
-    <div className="app-js-container">
-      <div className={styles.coinsMainText}>
-        <h1>IrishNoah Bit-Coin Information</h1>
-      </div>
-
-      <div className={styles.coins}>
-        {coin.map((data) => (
-          <div key={data.id}>
-            <div>
-              <p>Coin ID: {data.rank}</p>
-              <p>Coin ID: {data.id}</p>
-              <p>Coin Name: {data.name}</p>
-              <p>Symbol: {data.symbol}</p>
-              <p>First Data At: {data.first_data_at}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/detailCoin/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <HomeCoin />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default TrainFetchApi;
+export default App;
--- react-and-api/src/routes/HomeCoin.js
+++ react-and-api/src/routes/HomeCoin.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "./HomeCoin.module.css";
 
 function TrainFetchApi() {
   const [coin, setCoin] = useState(null);
@@ -9,8 +10,8 @@ function TrainFetchApi() {
       try {
         const response = await fetch("https://api.coinpaprika.com/v1/tickers");
         const coins = await response.json();
-        setCoin(coins[0]);
-        console.log(coins[0]);
+
+        setCoin(coins); // >>> 모두 다 불러옴
       } catch (error) {
         setError(error);
         console.error("Error fetching coin data:", error);
@@ -29,14 +30,23 @@ function TrainFetchApi() {
   }
 
   return (
-    <div className="TrainFetchApi">
-      <div>
-        <h1>Hello Irish</h1>
+    <div className="HomeCoin-js-container">
+      <div className={styles.coinsMainText}>
+        <h1>IrishNoah Bit-Coin Information</h1>
       </div>
-      <div>
-        <p>Coin Name: {coin.name}</p>
-        <p>Symbol: {coin.symbol}</p>
-        <p>First Data At: {coin.first_data_at}</p>
+
+      <div className={styles.coins}>
+        {coin.map((data) => (
+          <div key={data.id} className={styles.eachCoin}>
+            <div>
+              <p>Coin ID: {data.rank}</p>
+              <p>Coin ID: {data.id}</p>
+              <p>Coin Name: {data.name}</p>
+              <p>Symbol: {data.symbol}</p>
+              <p>First Data At: {data.first_data_at}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
