3c3e9d45d75eedc91783b5955d84c6f01b64c0bb
Chang-Young Park <parkcy0630@gmail.com>
2024-08-19 23:32:41 +0900

    240819 - Change Title Text & Add Link to Title

--- react-and-api/src/components/Header.js
+++ react-and-api/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>IrishNoah Bit-Coin Information</h1>
+      <Link className="headerMenu" to={"/"}>
+        <h1>CoinMate</h1>
+      </Link>
+      {/* <h1>CoinMate</h1> */}
     </div>
   );
 }
